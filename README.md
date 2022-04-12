a1d8c3501ffd9b2330b1d9bc0592f7f26b8b9f91
mishti169 <manishapanchal169@gmail.com>
2022-04-12 12:21:19 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-zegg2p
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-zegg2p)
---
+++ src/Card.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Card.css';
+import Button from './Button';
+function Card(props) {
+  console.log('hello');
+
+  return (
+    <div
+      className="movie-list-container"
+      style={{
+        backgroungImage: `url(${
+          props.imgSrc || 'https://wallpaperaccess.com/full/288744.jpg'
+        })`,
+      }}
+    >
+      <div className="movie-list-container-element">
+        <div className="movie-detail-wrapper">
+          <span className="movie-rate">{props.rating}</span>
+          <span className="movie-title">{props.title}</span>
+          <div>
+            <span className="movie-yearGenre">{props.year},</span>
+            <span className="movie-yearGenre">{props.genre}</span>
+          </div>
+        </div>
+        <p className="movie-description">{props.description}</p>
+        <Button text="Watch thriller" />
+      </div>
+    </div>
+  );
+}
+export default Card;
